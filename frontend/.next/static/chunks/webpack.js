bedb12764b72813b6c46f7af112f9a4203331c0f
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2025-05-26 18:23:37 +0800

    File webpack updates

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -88,18 +88,6 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	!function() {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = function(module) {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				function() { return module['default']; } :
-/******/ 				function() { return module; };
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	!function() {
 /******/ 		// define getter functions for harmony exports
@@ -128,7 +116,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ed150f5526c9f0bb"; }
+/******/ 		__webpack_require__.h = function() { return "bfc1cfb33d6e031c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
