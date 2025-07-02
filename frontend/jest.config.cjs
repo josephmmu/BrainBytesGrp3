1667115efaaf8a24c1cfa5ee5437eeafde838516
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2025-07-03 04:59:39 +0800

    trying to implement swc

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,11 +1,12 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx'],
-};
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
